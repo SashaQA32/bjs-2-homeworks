66edba4f15e3934e5f14a35ab2cfdd670e14cd56
Александр Стрюков <sanyajump16@gmail.com>
2022-07-21 20:48:13 +0300

    Initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,45 @@
+"use strict"
+
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+let student1 = new Student("Max", "male", 19);
+let student2 = new Student("Jordan", "male", 21);
+let student3 = new Student("Anna", "female", 21);
 
-// ваш код для остальных методов
+
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
+};
+
+Student.prototype.addMark = function(mark) {
+    if (this.marks === undefined) {
+        this.marks = [mark];
+    } else {
+        this.marks.push(mark);
+    }
+};
+
+Student.prototype.addMarks = function(...marks) {
+    if (this.marks === undefined) {
+        this.marks = [...marks];
+    } else {
+        this.marks = [...this.marks, ...marks];
+    }
+};
+
+Student.prototype.getAverage = function() {
+    let sum = 0;
+    this.marks.forEach((item, index) => sum += item);
+    let avg = sum / this.marks.length;
+    return avg;
+};
+
+Student.prototype.exclude = function(reason) {
+    delete this.marks;
+    delete this.subject;
+    this.excluded = reason;
+}
